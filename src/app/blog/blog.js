e1fc0d0a87f87fbf04d4ec3664fcce681ce6ca68
Cindy Juarez <sceendy@gmail.com>
2015-09-15 13:35:46 -0500

    use regex to isolate slugs and use for single blog post pages -- broken

--- src/app/blog/blog.js
+++ src/app/blog/blog.js
@@ -3,29 +3,19 @@
 angular.module('sceendyApp')
 // BLOG Factory: accesses the API and returns the blog data to be used
 .factory('Blog', function($resource) {
-  var key = 'AIzaSyCqUv0mNrHN0cEB7gwqiWC0A0rs71lpwgE';
-  //var entireBlog = function() {
-  return $resource(
-    'https://www.googleapis.com/blogger/v3/blogs/5718631717220089292/posts?key=' + key + '&fields=kind,items(title, content, published, labels)', {
-      maxResults: 20
+  var blog = $resource(
+    'https://www.googleapis.com/blogger/v3/blogs/5718631717220089292/posts?&fields=kind,items(id,title,content,published,labels,url),nextPageToken',{
+      key: 'AIzaSyCqUv0mNrHN0cEB7gwqiWC0A0rs71lpwgE',
+      maxResults: 20,
+      //pageToken: '@nextPageToken'
     },
-  {
-    query: {
-      method: 'GET',
-      isArray: false
-    }
-  });
+    {
+      'query':  { method:'GET', isArray: true },
+  }).get();
+
+  return blog;
 })
-  //};
-  /*var singleEntry = function(){
-    return $resource('https://www.googleapis.com/blogger/v3/blogs/5718631717220089292/posts/:postId',
-  {
-    query: {
-      method: 'GET',
-      isArray: false
-    }
-  });
-};*/
+
 
 // BLOG Controller: assigns blog data some values
 .controller('blogController', function($scope, $routeParams, Blog, $sce, posts) {
@@ -33,17 +23,19 @@ angular.module('sceendyApp')
   $scope.postId = $routeParams.id;
 
   // create slug for blog entries
+  // then, for HTML >> slug(posts.items[postId].url)
   $scope.slug = function(postUrl){
-    if (posts.items && postUrl){
+    if ($scope.posts.items && postUrl){
       var urlPattern = /http:\/\/sceendy\.blogspot\.com\/\d{4}\/\d{2}\/([a-zA-z0-9\-]+)\.html/g;
-      var slug = urlPattern.exec(postUrl);
-      return slug[1];
+      var slugged = urlPattern.exec(postUrl);
+
+      return slugged[1];
     }
   };
 
   $scope.blogContent = function(postId) {
-    if (posts.items && postId){
-      return $sce.trustAsHtml(posts.items[postId].content);
+    if ($scope.posts.items && postId){
+      return $sce.trustAsHtml($scope.posts.items[postId].content);
     }
   };
 
