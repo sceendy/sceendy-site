00c3653d69f400ff7972a3690b9dd8dd82d9b3e8
Cindy Juarez <sceendy@gmail.com>
2015-09-18 11:43:42 -0500

    html5mode

--- src/app/blog/blog.js
+++ src/app/blog/blog.js
@@ -20,23 +20,21 @@ angular.module('sceendyApp')
 // BLOG Controller: assigns blog data some values
 .controller('blogController', function($scope, $routeParams, Blog, $sce, posts) {
   $scope.posts = posts;
-  $scope.postId = $routeParams.id;
+  $scope.id = $routeParams.id;
 
-  // create slug for blog entries
-  // then, for HTML >> slug(posts.items[postId].url)
+  // sanitize the html, allowing tags from blogger api
+  $scope.blogContent = function(id) {
+    if ($scope.posts.items && $scope.id){
+      return $sce.trustAsHtml($scope.posts.items[id].content);
+    }
+  };
+
+  // create slug for blog entries --> slug(post.url)
   $scope.slug = function(postUrl){
-    if ($scope.posts.items && postUrl){
+    if ($scope.posts.items && $scope.id){
       var urlPattern = /http:\/\/sceendy\.blogspot\.com\/\d{4}\/\d{2}\/([a-zA-z0-9\-]+)\.html/g;
       var slugged = urlPattern.exec(postUrl);
-
       return slugged[1];
     }
   };
-
-  $scope.blogContent = function(postId) {
-    if ($scope.posts.items && postId){
-      return $sce.trustAsHtml($scope.posts.items[postId].content);
-    }
-  };
-
 });
--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,11 @@
 'use strict';
 angular.module('sceendyApp', ['ngAnimate', 'ngTouch', 'ngResource', 'ngRoute', 'ngSanitize', 'truncate'])
 
-  .config(function($routeProvider) {
+  .config(function($routeProvider, $locationProvider, window) {
+    $locationProvider .hashPrefix('!');
+    if(window.history && window.history.pushState){
+      $locationProvider.html5Mode({enabled: true,  requireBase: false});
+    }
     $routeProvider
       .when('/', {
         templateUrl: 'app/views/home.html',
@@ -37,16 +41,16 @@ angular.module('sceendyApp', ['ngAnimate', 'ngTouch', 'ngResource', 'ngRoute', '
         controller: 'blogController',
         title: 'Blog Post',
         resolve: {
-          /*slug: function(Blog, $route, $routeParams, $rootScope){
-            var postUrl = JSON.stringify($route.current.params);
-            console.log('postUrl: ' + postUrl + $routeParams); // for testing
-            var urlPattern = /http:\/\/sceendy\.blogspot\.com\/\d{4}\/\d{2}\/([a-zA-z0-9\-]+)\.html/g;
-            var slugged = urlPattern.exec(postUrl);
-            return (0);
-          },*/
           posts: function(Blog) {
             return Blog;
-          }
+          }/*,
+          slug: function(Blog, $routeParams, $route){
+            var postUrl = JSON.stringify($route.current.params.slug);
+            console.log('current.params: ' + postUrl); // for testing
+            var urlPattern = /http:\/\/sceendy\.blogspot\.com\/\d{4}\/\d{2}\/([a-zA-z0-9\-]+)\.html/g;
+            var slugged = urlPattern.exec(postUrl);
+            return 0;
+          }*/
         }
       })
 
@@ -57,7 +61,6 @@ angular.module('sceendyApp', ['ngAnimate', 'ngTouch', 'ngResource', 'ngRoute', '
 
   // APP Controller: allows for dynamic title and generates current year for footer
   .controller('appController', function($rootScope, $scope) {
-
     $rootScope.$on('$routeChangeSuccess', function(event, current) {
       $rootScope.title = current.title;
     });
